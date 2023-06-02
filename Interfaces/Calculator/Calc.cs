8fa51dc7cd0b954d7ab5fcabd268add837635a5e
vmrozhkovsky <128725885+vmrozhkovsky@users.noreply.github.com>
2023-06-02 16:04:26 +0300

    Добавлен проект Calculator и код задания реализации интерфейса

---
+++ Interfaces/Calculator/Calc.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Calc : ICalc
+    {
+        public double Sum(double a, double b)
+        {
+            return a + b;
+        }
+        public void ReadUserNumbers(out double a, out double b)
+        {
+            Console.WriteLine("Введите первое число:");
+            double InputNumber1 = 0;
+            bool IsCorrect = true;
+            while (IsCorrect)
+            {
+                try
+                {
+                    InputNumber1 = Convert.ToDouble(Console.ReadLine());
+                    IsCorrect = false;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Введенено некорректное значение. Введите число:");
+                }
+            }
+            a = InputNumber1;
+            Console.WriteLine("Введите второе число:");
+            double InputNumber2 = 0;
+            IsCorrect = true;
+            while (IsCorrect)
+            {
+                try
+                {
+                    InputNumber2 = Convert.ToDouble(Console.ReadLine());
+                    IsCorrect = false;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Введенено некорректное значение. Введите число:");
+                }
+            }
+            b = InputNumber2;
+        }
+    }
+}
---
+++ Interfaces/Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ICalc calc = new Calc();
+            double a;
+            double b;
+            double c;
+            calc.ReadUserNumbers(out a, out b);
+            c = calc.Sum(a, b);
+            Console.WriteLine(c.ToString());
+        }
+    }
+}
